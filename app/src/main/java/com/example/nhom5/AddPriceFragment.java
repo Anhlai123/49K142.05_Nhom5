23d82360a480afe19424355c440cffc299aee6f4
Tran Le Phuong Linh <linhnu.2185@gmail.com>
2026-04-03 22:20:08 +0700

    layout

--- app/src/main/java/com/example/nhom5/AddPriceFragment.java
+++ app/src/main/java/com/example/nhom5/AddPriceFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -25,6 +26,26 @@ public class AddPriceFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        binding.pillAllScope.setOnClickListener(v -> setScopeSelected(true));
+        binding.pillSpecificScope.setOnClickListener(v -> setScopeSelected(false));
+
+        TextView[] dayPills = new TextView[] {
+                binding.dayT2, binding.dayT3, binding.dayT4,
+                binding.dayT5, binding.dayT6, binding.dayT7, binding.dayCn
+        };
+        for (TextView dayPill : dayPills) {
+            dayPill.setOnClickListener(v -> toggleDayPill((TextView) v));
+        }
+
+        setScopeSelected(true);
+        setDayPillState(binding.dayT2, false);
+        setDayPillState(binding.dayT3, false);
+        setDayPillState(binding.dayT4, false);
+        setDayPillState(binding.dayT5, false);
+        setDayPillState(binding.dayT6, false);
+        setDayPillState(binding.dayT7, false);
+        setDayPillState(binding.dayCn, false);
+
         binding.btnBack.setOnClickListener(v -> Navigation.findNavController(v).navigateUp());
         
         binding.btnCancel.setOnClickListener(v -> Navigation.findNavController(v).navigateUp());
@@ -33,6 +54,30 @@ public class AddPriceFragment extends Fragment {
             // Handle save logic
             Navigation.findNavController(v).navigateUp();
         });
+    }
+
+    private void setScopeSelected(boolean allScope) {
+        if (allScope) {
+            binding.pillAllScope.setBackgroundResource(R.drawable.bg_pill_active);
+            binding.pillAllScope.setTextColor(requireContext().getColor(R.color.primary));
+            binding.pillSpecificScope.setBackgroundResource(R.drawable.bg_pill_inactive);
+            binding.pillSpecificScope.setTextColor(requireContext().getColor(R.color.text_grey));
+        } else {
+            binding.pillAllScope.setBackgroundResource(R.drawable.bg_pill_inactive);
+            binding.pillAllScope.setTextColor(requireContext().getColor(R.color.text_grey));
+            binding.pillSpecificScope.setBackgroundResource(R.drawable.bg_pill_active);
+            binding.pillSpecificScope.setTextColor(requireContext().getColor(R.color.primary));
+        }
+    }
+
+    private void toggleDayPill(TextView textView) {
+        setDayPillState(textView, !textView.isSelected());
+    }
+
+    private void setDayPillState(TextView textView, boolean selected) {
+        textView.setSelected(selected);
+        textView.setBackgroundResource(selected ? R.drawable.bg_day_pill_active : R.drawable.bg_day_pill_inactive);
+        textView.setTextColor(requireContext().getColor(selected ? R.color.white : R.color.inactive));
     }
 
     @Override
